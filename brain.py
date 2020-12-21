aa6b03e024044aa80d99ec2faa7680c8a1946417
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-12-21 22:35:19 +0330

    Brain is being completed

---
+++ brain.py
@@ -0,0 +1,30 @@
+import nltk 
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer() 
+import numpy 
+import tflearn 
+import tensorflow 
+import random 
+import json 
+
+with open('intents.json') as file:
+    data = json.load(file) 
+
+words = []
+labels = [] 
+docs_x = []
+docs_y = []
+
+for intent in data['intents']:
+    for pattern in intent['patterns']: 
+        wrds = nltk.word_tokenize(pattern) 
+        words.extend(wrds)
+        docs_x.append(wrds), 
+        docs_y.append(intent["tag"])
+    
+    if intent['tag'] not in labels:
+        labels.append(intent['tag'])
+
+words = [stemmer.stem(w.lower()) for w in words if w != "?"] 
+words = sorted(list(set(words))) 
+labels = sorted(labels)
