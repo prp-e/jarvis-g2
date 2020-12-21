a41ece06a8313e60a68ea80f2fd702c88c46e0b6
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-12-21 22:54:03 +0330

    Bag of words finished

--- brain.py
+++ brain.py
@@ -33,4 +33,21 @@ training = []
 output = []
 out_empty = [0 for _ in range(len(labels))] 
 
-print(out_empty)
+for x, doc in enumerate(docs_x): 
+    bag = []
+    wrds = [stemmer.stem(w) for w.lower() in doc] 
+    
+    for w in words:
+        if w in wrds:
+            bag.append(1) 
+        else:
+            bag.append(0) 
+        
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1 
+
+    training.append(bag) 
+    output.append(output_row)
+
+training = numpy.array(training)
+output = numpy.array(output)
