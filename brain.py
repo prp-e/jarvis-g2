dae0ffd128a463da386e2ab18c41145d3cb1dfc8
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-12-26 16:42:29 +0330

    Back to normal :P

--- brain.py
+++ brain.py
@@ -61,18 +61,18 @@ except:
     with open('data.pickle', 'wb') as f: 
         pickle.dump((words, labels, training, output), f) 
 
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8) 
+net = tflearn.fully_connected(net, len(output[0]), activation='softmax') 
+net = tflearn.regression(net) 
+
+model = tflearn.DNN(net)
 
 
 try:
     model.load('model/jarvis_mind.tflearn') 
 except:
-    net = tflearn.input_data(shape=[None, len(training[0])])
-    net = tflearn.fully_connected(net, 8)
-    net = tflearn.fully_connected(net, 8) 
-    net = tflearn.fully_connected(net, len(output[0]), activation='softmax') 
-    net = tflearn.regression(net) 
-
-    model = tflearn.DNN(net)
     model.fit(training, output, n_epoch=10000, batch_size=8, show_metric=True)
     model.save('model/jarvis_mind.tflearn')
 
